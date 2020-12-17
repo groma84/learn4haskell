94e7555de8979f07e685cae8c54f67e51226a834
Martin Grotz <martin.grotz@gmx.de>
2020-12-17 21:56:59 +0100

    Chapter 4 Task 3

--- watch-tests.sh
+++ watch-tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rerun --dir "src" --dir "test" --pattern "**/*.{hs}" --exit --clear --name "learn4haskell Tests" -- make test-chapter3
+rerun --dir "src" --dir "test" --pattern "**/*.{hs}" --exit --clear --name "learn4haskell Tests" -- make test-chapter4
